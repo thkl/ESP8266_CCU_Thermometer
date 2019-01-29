7febeb74711c3af15c26c99d162f72161eade47d
Thomas Kluge <th.kluge@me.com>
2019-01-29 21:22:18 +0100

    maked serial debug optional set sleeptime to 1200 seconds ...

--- src/main.cpp
+++ src/main.cpp
@@ -14,17 +14,20 @@
 #include "Settings.h"
 
 // Time in seconds the ESP goes to sleep
-#define SLEEP_TIME 3600
+#define SLEEP_TIME 1200
 #define HOSTNAME "ESP_Thermometer"
 
 // data wire is connected with GPIO2:
-#define ONE_WIRE_BUS 2
+#define ONE_WIRE_BUS D4
 
 // if you want to monitor the battery level connect A0 thru 220kO with 3.3V
 #define BATT_LEVEL A0
 // if not set BATT_LEVEL to 0
 
 #define RESET_CONFIG_PIN 4
+
+// set to true if you want to send debug messages to serial port
+#define DEBUG false
 
 // setup one wire instance:
 OneWire oneWire(ONE_WIRE_BUS);
@@ -46,9 +49,10 @@ void setup()
   pinMode (RESET_CONFIG_PIN, INPUT);
   digitalWrite(LED_BUILTIN,HIGH);
 
+  wifiManager.setDebugOutput(DEBUG);
   // start serial debug port
-  Serial.begin(9600);
-  Serial.println("");
+  if (DEBUG == true) Serial.begin(9600);
+  if (DEBUG == true) Serial.println("");
 
   // start eeporom
   EEPROM.begin(512);
@@ -60,16 +64,16 @@ void setup()
     // load settings
     eepromLoad();
   } else {
-    Serial.println("---- ");
-    Serial.println("Reset button");
-    Serial.println("---- ");
+    if (DEBUG == true) Serial.println("---- ");
+    if (DEBUG == true) Serial.println("Reset button");
+    if (DEBUG == true) Serial.println("---- ");
   }
   // some debug
-  Serial.println("Settings are :");
-  Serial.print("ccu_ip :");
-  Serial.println(deviceSettings.ccu_ip);
-  Serial.print("Variable :");
-  Serial.println(deviceSettings.ccu_variable);
+  if (DEBUG == true) Serial.println("Settings are :");
+  if (DEBUG == true) Serial.print("ccu_ip :");
+  if (DEBUG == true) Serial.println(deviceSettings.ccu_ip);
+  if (DEBUG == true) Serial.print("Variable :");
+  if (DEBUG == true) Serial.println(deviceSettings.ccu_variable);
 
   //create wifi manager extra parametes
   ccu_ip =  new WiFiManagerParameter("CCU", "CCU IP", deviceSettings.ccu_ip.toString().c_str(), 40);
@@ -83,7 +87,7 @@ void setup()
 
   // check invalid IPs -> default settings
   if ((deviceSettings.ccu_ip == IPAddress(2,0,0,1)) || (deviceSettings.ccu_ip == IPAddress(0,0,0,0))) {
-      Serial.println("Invalid CCU IP");
+      if (DEBUG == true) Serial.println("Invalid CCU IP");
       // call the Config Mode
       wifiManager.startConfigPortal(HOSTNAME);
   } else {
@@ -92,13 +96,15 @@ void setup()
   }
   // initialize DS1820 sensor with 12 bit resolution:
   sensors.begin();
-  sensors.setResolution(12);
-  Serial.println("Query Sensor");
+  sensors.setResolution(9);
+  if (DEBUG == true) Serial.println("Query Sensor");
   queryTemperature();
   delay(500);
   queryBattery();
   delay(1000);
-  Serial.println("Going to sleep");
+
+  if (DEBUG == true) Serial.println("Going to sleep");
+
   // sleeptime are microseconds so we have to multiply
   ESP.deepSleep(SLEEP_TIME * 1000000);
 }
@@ -118,8 +124,8 @@ void queryBattery() {
     double  batteryLevel = analogRead(BATT_LEVEL);
     // calculate voltage based on a 220k resistor
     batteryLevel = batteryLevel / 188.31;
-    Serial.print("Battery :");
-    Serial.println(batteryLevel);
+    if (DEBUG == true) Serial.print("Battery :");
+    if (DEBUG == true) Serial.println(batteryLevel);
     // replace values
     requestUrl.replace("{ccuip}", deviceSettings.ccu_ip.toString());
     requestUrl.replace("{varname}", String(deviceSettings.ccu_variable));
@@ -132,8 +138,8 @@ void queryBattery() {
 // make a http call
 void regaCall(String url) {
   HTTPClient http;
-  Serial.print("Url is:");
-  Serial.println(url);
+  if (DEBUG == true) Serial.print("Url is:");
+  if (DEBUG == true) Serial.println(url);
   // send request
   http.begin(url);
   http.GET();
@@ -145,9 +151,19 @@ void regaCall(String url) {
 // request temperature from sensor and send this to ccu
 void queryTemperature() {
   float temperature;
+
+  sensors.setResolution(9);
+  // confirm that we set that resolution by asking the DS18B20 to repeat it back
+  if (DEBUG == true) Serial.print("Sensor Resolution: ");
+  if (DEBUG == true) Serial.println(sensors.getResolution(), DEC);
+  if (DEBUG == true) Serial.println();
+
+
   // get the temperature
   sensors.requestTemperatures();
+  delay(500);
   temperature = sensors.getTempCByIndex(0);
+  if (DEBUG == true) Serial.println(temperature);
   // ccu set variable template
   String requestUrl = "http://{ccuip}:8181/tclrega.exe?x=dom.GetObject(ID_SYSTEM_VARIABLES).Get(\"{varname}\").State({temp});";
   // replace variables
@@ -159,7 +175,7 @@ void queryTemperature() {
 
 void saveConfigCallback () {
   IPAddress addr;
-  Serial.println("Saving :");
+  if (DEBUG == true) Serial.println("Saving :");
   // generate ip from WifiManager Parameter
   if (addr.fromString(ccu_ip->getValue())) {
     deviceSettings.ccu_ip = addr;
@@ -168,10 +184,10 @@ void saveConfigCallback () {
   //copy variable name
   strcpy(deviceSettings.ccu_variable,ccu_variable->getValue());
 
-  Serial.print("ccu_ip :");
-  Serial.println(deviceSettings.ccu_ip);
-  Serial.print("Variable :");
-  Serial.println(deviceSettings.ccu_variable);
+  if (DEBUG == true) Serial.print("ccu_ip :");
+  if (DEBUG == true) Serial.println(deviceSettings.ccu_ip);
+  if (DEBUG == true) Serial.print("Variable :");
+  if (DEBUG == true) Serial.println(deviceSettings.ccu_variable);
 
   // save to eeprom
   eepromSave();
